6c139f579cbd2babee5d17856e57a60d80f7052e
Samuel Stark <samuel@winslowstark.org>
2019-09-11 19:22:26 +0100

    Cleanup, moved register and initial state detection to separate function

--- dxbc/v2/disassembly/Disassembler.py
+++ dxbc/v2/disassembly/Disassembler.py
@@ -4,7 +4,7 @@ from itertools import chain
 from dxbc.v2.program.Functions import *
 from dxbc.v2.program.State import ProgramState
 from dxbc.v2.disassembly.Tokenizer import Tokenizer, INSTRUCTION_TOKEN, NewlineToken, WhitespaceToken, \
-    SHADER_START_TOKEN, DECLARATION_TOKEN, NameToken
+    SHADER_START_TOKEN, DECLARATION_TOKEN, NameToken, Token
 from dxbc.v2.program.Variables import *
 from dxbc.v2.values.Utils import get_type_string
 from dxbc.v2.values.tokens import ValueToken
@@ -81,6 +81,9 @@ class Disassembler:
         self.instructions = []
         self.initial_state = None # ProgramState(initial_scalar_types, initial_vec_state)
 
+    def get_disassembled_shader(self) -> str:
+        raise NotImplementedError
+
     def get_function_contents_hlsl(self, line_prefix: str = ""):
         function_contents = ""
         for action in self.actions:
@@ -95,6 +98,9 @@ class Disassembler:
             function_contents += line_disassembly
         return function_contents
 
+    def get_declaration_hlsl(self, line_prefix: str = "") -> str:
+        raise NotImplementedError()
+
     def disassemble_file(self, file_contents: str):
         tokens, remaining = self.tokenizer.tokenize_file(file_contents)
         tokens = [x for x in tokens if not isinstance(x, (WhitespaceToken, NewlineToken))]
@@ -102,108 +108,10 @@ class Disassembler:
             raise DXBCError("Expected SHADER_START_TOKEN at the beginning of the file")
 
         declaration_tokens = [x for x in tokens if isinstance(x, DECLARATION_TOKEN)]
-        declaration_pairs = [(NameToken.from_type(type(x.tokens[1])), x) for x in declaration_tokens]
-        declaration_dict = {e: [x for x_e, x in declaration_pairs if x_e == e] for e in NameToken}
+        self.initial_state, self.registers = self.generate_initial_state(declaration_tokens)
 
-        initial_types: Dict[VarNameBase, ScalarType] = {}
-        initial_vector_state: Dict[VarNameBase, int] = {}
-        scalar_variable_names: List[VarNameBase] = []
-
-        for x in declaration_dict[NameToken.ImmediateBufferToken]:
-            # TODO: Assume immediate constant buffer is float4x4
-            initial_types[VarNameBase("icb")] = ScalarType.Float
-            initial_vector_state[VarNameBase("icb")] = 4
-            break # Only expect one
-        for x in declaration_dict[NameToken.ConstantBufferToken]:
-            arg_tokens = [t for t in x.tokens[3].tokens if hasattr(t, "value")]
-            if (len(arg_tokens) == 0
-                    or not isinstance(arg_tokens[0].value, ScalarVariable)
-                    or not isinstance(arg_tokens[0].value.scalar_name, IndexedVarName)):
-                print(x.tokens[3].tokens)
-                raise DXBCError(
-                    f"Expected constant buffer token to be of the form 'name[length]', got '{x.tokens[3].str_data}'")
-            base_name = arg_tokens[0].value.scalar_name.as_base()
-            initial_types[base_name] = ScalarType.Float
-            initial_vector_state[base_name] = 4
-        for x in (declaration_dict[NameToken.SamplerToken] + declaration_dict[NameToken.TextureToken]):
-            arg_tokens = [t for t in x.tokens[3].tokens if hasattr(t, "value")]
-            if (len(arg_tokens) == 0
-                    or not isinstance(arg_tokens[0].value, ScalarVariable)
-                    or not type(arg_tokens[0].value.scalar_name) == VarNameBase):
-                raise DXBCError(
-                    f"Expected texture/sampler token to be of the form 'nameN', got '{x.tokens[3].str_data}'")
-            base_name = arg_tokens[0].value.scalar_name
-            initial_types[base_name] = ScalarType.Untyped
-            if "t" in base_name.name:
-                initial_vector_state[base_name] = 4 # Textures are accessed as vectors in assembly
-            else:
-                scalar_variable_names.append(base_name)
-        for x in (declaration_dict[NameToken.TypedPSInput]
-                  + declaration_dict[NameToken.UntypedInput]
-                  + declaration_dict[NameToken.Output]):
-            arg_tokens = [t for t in x.tokens[3].tokens if hasattr(t, "value")]
-            if len(arg_tokens) == 0:
-                raise DXBCError(
-                    f"Expected argument after input/output declaration, got '{x.tokens[3].str_data}'")
-
-            base_name: VarNameBase
-            if isinstance(arg_tokens[0].value, ScalarVariable):
-                base_name = arg_tokens[0].value.scalar_name
-                component_count = 1
-            elif isinstance(arg_tokens[0].value, SwizzledVectorValue):
-                base_name = arg_tokens[0].value.vector_name
-                component_count = max(arg_tokens[0].value.components) + 1
-            elif isinstance(arg_tokens[0].value, SingleVectorComponent):
-                base_name = arg_tokens[0].value.vector_name
-                component_count = arg_tokens[0].value.component_name + 1
-            else:
-                raise DXBCError(
-                    f"Expected input/output declaration to declare a ScalarVariable "
-                    f", SwizzledVectorValue, or SingleVectorComponent, got {type(arg_tokens[0].value)}")
-
-            if component_count == 3:
-                component_count = 4 # Pad to pow2
-
-            initial_types[base_name] = ScalarType.Uint if "Mask" in base_name.name else ScalarType.Float
-            if base_name.name == "vCoverageMask":
-                component_count = 4
-            if component_count > 1:
-                initial_vector_state[base_name] = component_count
-            else:
-                scalar_variable_names.append(base_name)
-
-        #print(scalar_variable_names)
-        #print(initial_vector_state)
-
-        for x in declaration_dict[NameToken.RegisterCount]:
-            arg_tokens = [t for t in x.tokens[3].tokens if hasattr(t, "value")]
-            if (len(arg_tokens) == 0
-                    or not isinstance(arg_tokens[0].value, ImmediateScalar)):
-                print(arg_tokens)
-                raise DXBCError(
-                    f"Expected register count to be an immediate, got '{x.tokens[3].str_data}'")
-            register_count = arg_tokens[0].value.value
-            self.registers = [f"r{i}" for i in range(0, register_count)]
-            break
-
-        #if not self.registers:
-        #    raise DXBCError("Expected RegisterCount declaration")
-
-        initial_scalar_types: Dict[ScalarID, VariableState] = {
-            ScalarID(name): VariableState(None, initial_types[name])
-            for name in scalar_variable_names
-        }
-        #print(initial_scalar_types)
-        initial_scalar_types.update({
-            ScalarID(name, comp): VariableState(None, t)
-            for (name, t) in initial_types.items() if name not in scalar_variable_names
-            for comp in VectorComponent if comp < initial_vector_state[name]
-        })
-
-        self.initial_state = ProgramState(initial_scalar_types, initial_vector_state)
-
-        for e in NameToken:
-            print(f"{e.name}: {[x.tokens[3].str_data for x in declaration_dict[e]]}")
+        #for e in NameToken:
+        #    print(f"{e.name}: {[x.tokens[3].str_data for x in declaration_dict[e]]}")
 
         current_tick: int = 0
         current_state: ProgramState = self.initial_state
@@ -264,6 +172,107 @@ class Disassembler:
                 break
 
             current_tick += 1
+
+    def generate_initial_state(self, declaration_tokens: List[DECLARATION_TOKEN]) -> Tuple[ProgramState, List[str]]:
+        declaration_pairs = [(NameToken.from_type(type(x.tokens[1])), x) for x in declaration_tokens]
+        declaration_dict = {e: [x for x_e, x in declaration_pairs if x_e == e] for e in NameToken}
+
+        initial_types: Dict[VarNameBase, ScalarType] = {}
+        initial_vector_state: Dict[VarNameBase, int] = {}
+        scalar_variable_names: List[VarNameBase] = []
+
+        for x in declaration_dict[NameToken.ImmediateBufferToken]:
+            # TODO: Assume immediate constant buffer is float4x4
+            initial_types[VarNameBase("icb")] = ScalarType.Float
+            initial_vector_state[VarNameBase("icb")] = 4
+            break  # Only expect one
+
+        for x in declaration_dict[NameToken.ConstantBufferToken]:
+            arg_tokens = [t for t in x.tokens[3].tokens if hasattr(t, "value")]
+            if (len(arg_tokens) == 0
+                    or not isinstance(arg_tokens[0].value, ScalarVariable)
+                    or not isinstance(arg_tokens[0].value.scalar_name, IndexedVarName)):
+                raise DXBCError(
+                    f"Expected constant buffer token to be of the form 'name[length]', got '{x.tokens[3].str_data}'")
+            base_name = arg_tokens[0].value.scalar_name.as_base()
+            initial_types[base_name] = ScalarType.Float
+            initial_vector_state[base_name] = 4
+
+        for x in (declaration_dict[NameToken.SamplerToken] + declaration_dict[NameToken.TextureToken]):
+            arg_tokens = [t for t in x.tokens[3].tokens if hasattr(t, "value")]
+            if (len(arg_tokens) == 0
+                    or not isinstance(arg_tokens[0].value, ScalarVariable)
+                    or not type(arg_tokens[0].value.scalar_name) == VarNameBase):
+                raise DXBCError(
+                    f"Expected texture/sampler token to be of the form 'nameN', got '{x.tokens[3].str_data}'")
+            base_name = arg_tokens[0].value.scalar_name
+            initial_types[base_name] = ScalarType.Untyped
+            if "t" in base_name.name:
+                initial_vector_state[base_name] = 4  # Textures are accessed as vectors in assembly
+            else:
+                scalar_variable_names.append(base_name)
+
+        for x in (declaration_dict[NameToken.TypedPSInput]
+                  + declaration_dict[NameToken.UntypedInput]
+                  + declaration_dict[NameToken.Output]):
+            arg_tokens = [t for t in x.tokens[3].tokens if hasattr(t, "value")]
+            if len(arg_tokens) == 0:
+                raise DXBCError(
+                    f"Expected argument after input/output declaration, got '{x.tokens[3].str_data}'")
+
+            base_name: VarNameBase
+            if isinstance(arg_tokens[0].value, ScalarVariable):
+                base_name = arg_tokens[0].value.scalar_name
+                component_count = 1
+            elif isinstance(arg_tokens[0].value, SwizzledVectorValue):
+                base_name = arg_tokens[0].value.vector_name
+                component_count = max(arg_tokens[0].value.components) + 1
+            elif isinstance(arg_tokens[0].value, SingleVectorComponent):
+                base_name = arg_tokens[0].value.vector_name
+                component_count = arg_tokens[0].value.component_name + 1
+            else:
+                raise DXBCError(
+                    f"Expected input/output declaration to declare a ScalarVariable "
+                    f", SwizzledVectorValue, or SingleVectorComponent, got {type(arg_tokens[0].value)}")
+
+            if component_count == 3:
+                component_count = 4  # Pad to pow2
+
+            initial_types[base_name] = ScalarType.Uint if "Mask" in base_name.name else ScalarType.Float
+            if base_name.name == "vCoverageMask":
+                component_count = 4
+            if component_count > 1:
+                initial_vector_state[base_name] = component_count
+            else:
+                scalar_variable_names.append(base_name)
+
+        initial_scalar_types: Dict[ScalarID, VariableState] = {
+            ScalarID(name): VariableState(None, initial_types[name])
+            for name in scalar_variable_names
+        }
+        initial_scalar_types.update({
+            ScalarID(name, comp): VariableState(None, t)
+            for (name, t) in initial_types.items() if name not in scalar_variable_names
+            for comp in VectorComponent if comp < initial_vector_state[name]
+        })
+        initial_state = ProgramState(initial_scalar_types, initial_vector_state)
+
+        registers = []
+        for x in declaration_dict[NameToken.RegisterCount]:
+            arg_tokens = [t for t in x.tokens[3].tokens if hasattr(t, "value")]
+            if (len(arg_tokens) == 0
+                    or not isinstance(arg_tokens[0].value, ImmediateScalar)):
+                print(arg_tokens)
+                raise DXBCError(
+                    f"Expected register count to be an immediate, got '{x.tokens[3].str_data}'")
+            register_count = arg_tokens[0].value.value
+            registers = [f"r{i}" for i in range(0, register_count)]
+            break
+
+        # if not registers:
+        #    raise DXBCError("Expected RegisterCount declaration")
+
+        return initial_state, registers
 
     def update_state(self, function: Function, input_vals: List[Value], output_value: Value,
                      current_state: ProgramState):
--- main.py
+++ main.py
@@ -5,4 +5,5 @@ from shader_source import ps_instruction_str as instruction_str
 disassembler = Disassembler()
 disassembler.disassemble_file(instruction_str)
 #disassembler.disassemble_program_contents(instruction_str)
-print(disassembler.get_function_contents_hlsl(line_prefix="\t"))
+#print(disassembler.get_function_contents_hlsl(line_prefix="\t"))
+print(disassembler.get_disassembled_shader())
